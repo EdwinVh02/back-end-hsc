dcb1355a9ef42e8d25892bb017fdcb1c1232ae83
Caih2023 <estadiacaih@gmail.com>
2024-03-22 07:25:04 -0600

    prueba

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/api/rooms", roomRoutes);
 app.use("/api/auth", authRoutes);
-
+//hola
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
