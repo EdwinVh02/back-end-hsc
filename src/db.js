d600e1cc7e695324a2080ea5f0b4ffbf52cfa1b4
Caih2023 <estadiacaih@gmail.com>
2024-04-08 13:33:29 -0600

    db

--- src/db.js
+++ src/db.js
@@ -1,19 +1,15 @@
-
-require("dotenv").config();
 const sql = require("mssql");
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  user: "sa",
+  password: "2402",
+  server: "localhost",
+  database: "hotelsc",
   options: {
     encrypt: false,
     trustServerCertificate: false,
   },
 };
-
 
 const pool = new sql.ConnectionPool(config);
 
